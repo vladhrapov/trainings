9a6d84a52ad8d9d528e1598f0bef60787217cd9b
Vladyslav Khrapov <vlady@Vladyslavs-MacBook-Pro-2.local>
2018-11-14 22:10:01 +0200

    Add asp net overview

---
+++ asp_net_overview/materials/001_state/aspnet_state/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using aspnet_state.Models;
+
+namespace aspnet_state.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            HttpContext.Session.GetString("car-data");
+            return View();
+        }
+
+        public IActionResult Cookies()
+        {
+            ViewBag.Message = "Cookies";
+
+            HttpContext.Response.Cookies.Append("user-name", "vasya");
+
+            return View();
+        }
+
+        public IActionResult Session()
+        {
+            ViewData["Message"] = "Session";
+
+            HttpContext.Session.SetString("car-data", "Ferrari");
+            HttpContext.Session.SetInt32("car-year-data", 2018);
+
+            return View();
+        }
+
+        public IActionResult Application()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ asp_net_overview/materials/002_state_fe/aspnet_state_fe/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using aspnet_state_fe.Models;
+
+namespace aspnet_state_fe.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Segments(int id)
+        {
+            ViewBag.passedId = id;
+
+            return View("Index");
+        }
+
+        public IActionResult SegmentsWithQueryString(int id, string data)
+        {
+            ViewBag.passedId = id;
+            ViewBag.data = data;
+
+            return View("Index");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
